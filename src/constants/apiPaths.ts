96c298c2ab346c57adc54007d921b8c6cb2f7941
Konstantin Dedulya <eximal@gmail.com>
2021-08-22 01:07:54 +0300

    refactor: change api url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: 'https://gpfawhrau3.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://gpfawhrau3.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://gpfawhrau3.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://gpfawhrau3.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://gpfawhrau3.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://0mebc4spsh.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://0mebc4spsh.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://0mebc4spsh.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://0mebc4spsh.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://0mebc4spsh.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
