8e5044b209c9a606f215213072cb1c0584d0bc54
Konstantin Dedulya <eximal@gmail.com>
2021-09-04 12:11:58 +0300

    refactor: change API url after missclick

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: 'https://0mebc4spsh.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://0mebc4spsh.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://0mebc4spsh.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://0mebc4spsh.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://0mebc4spsh.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://oa7vk8zo9h.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://oa7vk8zo9h.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://oa7vk8zo9h.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://oa7vk8zo9h.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://oa7vk8zo9h.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
